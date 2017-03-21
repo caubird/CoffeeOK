4a9a042cca72ace1bddb40cfda1ff53dc29fe3c3
caubird <lijianyou77@gmail.com>
2017-03-21 15:51:02 +0800

    no message

---
+++ Coffee_OK_0319/Coffee_OK_0308/CoffeeOK_Control_0307/CoffeeOK_ControlForm/CoffeeOK_ControlForm/CoffeeOK_Control.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace CoffeeOK_ControlForm
+{
+    partial class CoffeeOK_Control
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.RobertComPort_comboBox = new System.Windows.Forms.ComboBox();
+            this.RobertComPort_Label = new System.Windows.Forms.Label();
+            this.ControlBoardComPort_Label = new System.Windows.Forms.Label();
+            this.ControlBoardComPort_comboBox = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.DB_CheckTimer = new System.Windows.Forms.Timer(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.DB_InsertTimer = new System.Windows.Forms.Timer(this.components);
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RobertComPort_comboBox
+            // 
+            this.RobertComPort_comboBox.FormattingEnabled = true;
+            this.RobertComPort_comboBox.Location = new System.Drawing.Point(12, 77);
+            this.RobertComPort_comboBox.Name = "RobertComPort_comboBox";
+            this.RobertComPort_comboBox.Size = new System.Drawing.Size(121, 20);
+            this.RobertComPort_comboBox.TabIndex = 0;
+            // 
+            // RobertComPort_Label
+            // 
+            this.RobertComPort_Label.AutoSize = true;
+            this.RobertComPort_Label.Location = new System.Drawing.Point(13, 59);
+            this.RobertComPort_Label.Name = "RobertComPort_Label";
+            this.RobertComPort_Label.Size = new System.Drawing.Size(113, 12);
+            this.RobertComPort_Label.TabIndex = 1;
+            this.RobertComPort_Label.Text = "选择机器人通信串口";
+            // 
+            // ControlBoardComPort_Label
+            // 
+            this.ControlBoardComPort_Label.AutoSize = true;
+            this.ControlBoardComPort_Label.Location = new System.Drawing.Point(12, 142);
+            this.ControlBoardComPort_Label.Name = "ControlBoardComPort_Label";
+            this.ControlBoardComPort_Label.Size = new System.Drawing.Size(101, 12);
+            this.ControlBoardComPort_Label.TabIndex = 3;
+            this.ControlBoardComPort_Label.Text = "选择控制电控串口";
+            // 
+            // ControlBoardComPort_comboBox
+            // 
+            this.ControlBoardComPort_comboBox.FormattingEnabled = true;
+            this.ControlBoardComPort_comboBox.Location = new System.Drawing.Point(11, 160);
+            this.ControlBoardComPort_comboBox.Name = "ControlBoardComPort_comboBox";
+            this.ControlBoardComPort_comboBox.Size = new System.Drawing.Size(121, 20);
+            this.ControlBoardComPort_comboBox.TabIndex = 2;
+            this.ControlBoardComPort_comboBox.SelectedIndexChanged += new System.EventHandler(this.ControlBoardComPort_comboBox_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(15, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 335);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Interval = 1000;
+            this.MainTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Setp1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(173, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Setp2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(173, 172);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Setp3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(173, 201);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Setp4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(173, 324);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Read";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // DB_CheckTimer
+            // 
+            this.DB_CheckTimer.Interval = 1000;
+            this.DB_CheckTimer.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(15, 324);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "电控测试";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(173, 74);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(111, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Start port";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(521, 324);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 23);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "SetDB";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(521, 191);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(111, 23);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "TestTask";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(173, 230);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(111, 23);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "Setp5";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(173, 259);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(111, 23);
+            this.button12.TabIndex = 16;
+            this.button12.Text = "Setp6";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // DB_InsertTimer
+            // 
+            this.DB_InsertTimer.Interval = 1000;
+            this.DB_InsertTimer.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(521, 35);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(111, 23);
+            this.button13.TabIndex = 17;
+            this.button13.Text = "FarCodeTest";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(521, 77);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(111, 23);
+            this.button14.TabIndex = 18;
+            this.button14.Text = "PowerOff_Test";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // CoffeeOK_Control
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 374);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ControlBoardComPort_Label);
+            this.Controls.Add(this.ControlBoardComPort_comboBox);
+            this.Controls.Add(this.RobertComPort_Label);
+            this.Controls.Add(this.RobertComPort_comboBox);
+            this.Name = "CoffeeOK_Control";
+            this.Text = "CoffeeOK";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Enter += new System.EventHandler(this.Form1_Enter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox RobertComPort_comboBox;
+        private System.Windows.Forms.Label RobertComPort_Label;
+        private System.Windows.Forms.Label ControlBoardComPort_Label;
+        private System.Windows.Forms.ComboBox ControlBoardComPort_comboBox;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer DB_CheckTimer;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Timer DB_InsertTimer;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+    }
+}
+
---
+++ Coffee_OK_0319/Coffee_OK_0308/CoffeeOK_Control_0307/CoffeeOK_ControlForm/CoffeeOK_ControlForm/ConnectControlBoard.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace CoffeeOK_ControlForm
+{
+    class ConnectControlBoard
+    {
+
+        #region 命令帧汇总
+
+        public byte[] Order_ControlBoard_ActionInit = { 0x55, 0xAA, 0x02, 0x04, 0x00, 0x01, 0x00 };
+        public byte[] Order_ControlBoard_ReleaseACup = { 0x55, 0xAA, 0x02, 0x04, 0x00, 0x01, 0x01 };
+        public byte[] Order_ControlBoard_ChangeCupQueue = { 0x55, 0xAA, 0x02, 0x04, 0x00, 0x01, 0x02 };
+        public byte[] Order_ControlBoard_AddSeasoning1 = { 0x55, 0xAA, 0x02, 0x04, 0x00, 0x01, 0x03 };
+        public byte[] Order_ControlBoard_AddSeasoning2 = { 0x55, 0xAA, 0x02, 0x04, 0x00, 0x01, 0x04 };
+        public byte[] Order_ControlBoard_AddSeasoning3 = { 0x55, 0xAA, 0x02, 0x04, 0x00, 0x01, 0x05 };
+        public byte[] Order_ControlBoard_AddSeasoning4 = { 0x55, 0xAA, 0x02, 0x04, 0x00, 0x01, 0x06 };
+
+        public byte[] Order_ControlBoard_ReplyOK = { 0x55, 0xAA, 0x02, 0x04, 0x00, 0x01, 0x00 };
+
+        #endregion 命令帧汇总
+        #region 变量声明
+        public System.IO.Ports.SerialPort ComPort = new System.IO.Ports.SerialPort();
+        public ConnectControlBoard()
+        {
+            ReadTimer.Elapsed += ReadTimer_Elapsed;
+            WriteTimer.Elapsed += WriteTimer_Elapsed;
+            
+
+
+        }
+        List<byte[]> WriteFrameList = new List<byte[]>();
+        List<byte[]> ReadFrameList = new List<byte[]>();
+        int ResponseBytesPoint = 0;
+        byte[] LastSend = new byte[0];
+        public string ComPortName = "";
+
+        public List<FrameItem> FrameList = new List<FrameItem>();
+
+        public static byte CRC_Compute(byte[] data, int dataLen)
+        {
+            byte chk_sum = 0; // initialize check sum;
+            byte[] ptr = data;
+
+            for (int i = 0; i < dataLen; i++)
+            {
+                chk_sum ^= data[i];
+            }
+
+            return chk_sum;
+        }
+
+        //public System.IO.Ports.SerialPort ComPort = new System.IO.Ports.SerialPort();
+
+        public byte[] OgnizeReportFrame(byte[] ReportItem)
+        {
+            int length = 7 + ReportItem.Length;
+            byte[] result = new byte[length];
+
+            FrameItem Frame = new FrameItem();
+            Frame.ReportItem = ReportItem;
+
+            result[0] = Frame.HeadByte[0];
+            result[1] = Frame.HeadByte[1];
+            result[2] = Frame.FrameType;
+            result[3] = Frame.ReportType;
+            result[4] = Frame.FrameLenth[0];
+            result[5] = Frame.FrameLenth[1];
+            for (int i = 0; i < Frame.ReportItem.Length; i++)
+            {
+                result[i] = Frame.ReportItem[i];
+            }
+            result[6 + Frame.ReportItem.Length] = CRC_Compute(result, result.Length - 1);
+            return result;
+        }
+
+        public System.Timers.Timer ReadTimer = new System.Timers.Timer();
+
+        public System.Timers.Timer WriteTimer = new System.Timers.Timer();
+
+        byte[] DataReadBack = new byte[2000];
+        int DataReadCount = 0;
+        #endregion
+        public void InitComPort(string portname)
+        {
+            //ComPort.PortName = ComPortName;
+           // ComPort.BaudRate = 4800;
+            ComPort.DataBits = 8;
+            ComPort.StopBits = System.IO.Ports.StopBits.One;
+            ComPort.Parity = System.IO.Ports.Parity.None;
+            ComPort.Encoding = Encoding.UTF8;
+            ComPort.BaudRate = 115200;
+            ComPort.PortName = portname;
+            ComPort.Open();
+        }
+        public void StartConnection()
+        {
+            ComPort.Open();//打开端口
+            ReadTimer.Interval = 100;//100ms
+            WriteTimer.Interval = 100;//100ms
+            WriteTimer.Start();
+            ReadTimer.Start();
+        }
+
+        public void WriteRobot(byte[] inputstring)
+        {
+            //if (Conn.State == ConnectionState.Closed)
+            //       Conn.Open();
+            byte[] bytes = inputstring;// Encoding.UTF8.GetBytes(inputstring);//inputstring.Split(' ').Select(s => Convert.ToByte(s, 16)).ToArray();
+            byte[] SHOW = bytes;
+            byte crc = CRC_Compute(bytes,bytes.Length );
+            byte[] AddSHOW = new byte[SHOW.Length + 1];
+            for (int i = 0; i < SHOW.Length; i++)
+            {
+                AddSHOW[i] = SHOW[i];
+            }
+            //AddSHOW[AddSHOW.Length - 2] = crc[0];
+            AddSHOW[AddSHOW.Length - 1] = crc;
+            ComPort.Write(AddSHOW, 0, AddSHOW.Length);
+            Thread.Sleep(200);
+            byte[] recivedata = new byte[ComPort.BytesToRead];
+            if (ComPort.BytesToRead >= 5)
+            {
+                bytes = new byte[ComPort.BytesToRead];
+                ComPort.Read(recivedata, 0, recivedata.Length);
+                byte temp = CRC_Compute(recivedata,recivedata.Length);
+                if (temp == recivedata[recivedata.Length - 1] )//注意高低位调换
+                {
+                    
+                    ReadFrameList.Insert(ResponseBytesPoint,recivedata) ;
+                    ResponseBytesPoint++;
+                    //Console.ReadLine();
+                }
+                else
+                {
+                    //Console.WriteLine("接收数据错误");
+                }
+            }
+        }
+        private void WriteTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (ComPort.IsOpen && WriteFrameList.Count > 0)
+            {
+                //byte bytes=new byte [[0].]
+                ComPort.Write(WriteFrameList[0], 0, WriteFrameList[0].Length);
+                LastSend = WriteFrameList[0];
+                WriteFrameList.RemoveAt(0);//.Remove(WriteFrameList[0])
+            }
+        }
+
+        private void ReadTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (ComPort.BytesToRead > 0)
+            {
+                int counter = ComPort.BytesToRead;
+                for (int i = 0; i < counter; i++)
+                {
+                    DataReadBack[DataReadCount] = (byte)ComPort.ReadByte();
+                    DataReadCount++;
+                }
+            }
+            if (DataReadCount > 7)
+            {//满足解析的必要条件
+                //
+                for (int i = 0; (i + 1) < DataReadCount; i++)
+                {
+                    if (DataReadBack[i] == 0x55 && DataReadBack[i + 1] == 0xAA && (DataReadCount - i) >= 7)
+                    {
+                        FrameCopy(i);
+                        ReorgnizeBuf(i);
+                    }
+                    else if ((DataReadCount - i) < 7)
+                    {
+                        //未接受完，继续接收等待
+                    }
+                }
+            }
+        }
+
+        #region 接收数据实际处理部分
+        /// <summary>
+        /// 将一帧数据从接收缓冲区抓取出来，并加入接收队列，等待处理
+        /// </summary>
+        /// <param name="Index"></param>
+        private void FrameCopy(int Index)
+        {
+            //找到帧长度，根据帧长度判定本帧数据是否接收完成
+            int length = DataReadBack[4 + Index] * 256 + DataReadBack[5 + Index];
+            if (DataReadCount >= length + 7)
+            {
+                byte CRC_Received = DataReadBack[Index + length + 6];
+                if (CRC_Received == CRC_Compute(DataReadBack, length + 6))
+                {
+                    byte[] framebytes = new byte[length + 7];
+                    for (int i = 0; i < length + 7; i++)
+                    {
+                        framebytes[i] = DataReadBack[Index + i];
+                    }
+                    ReadFrameList.Add(framebytes);//把一帧加入到接收列表
+                }
+            }
+        }
+        private void ReorgnizeBuf(int Index)
+        {
+            int length = DataReadBack[4 + Index] * 256 + DataReadBack[5 + Index];
+            if (DataReadBack.Length >= Index + 6 + length)
+            {
+                ///移动后面内容到缓冲区前部分，前面部分内容覆盖
+                for (int i = Index + 6 + length; i <= DataReadBack.Length; i++)
+                {
+                    DataReadBack[i - Index - 6 - length] = DataReadBack[Index];
+
+                }
+                //接收缓冲区标记长度缩减
+                DataReadCount -= Index + 6 + length;
+            }
+
+        }
+
+        public byte[] ReadBackBytes(SerialPort sp1)
+        {
+            if (sp1.IsOpen)
+            {
+                return null;
+            }
+            int counter = sp1.BytesToRead;
+            byte[] bytes = new byte[counter];
+            sp1.Read(bytes, 0, counter);
+
+            return bytes;
+        }
+        /// <summary>
+        /// 协议内容分析
+        /// </summary>
+        private void FrameAnalysis()
+        {
+            for (int i = 0; i < FrameList.Count; i++)
+            {
+                byte[] FrameBytes = ReadFrameList[i];
+                byte[] ReportItem = new byte[FrameBytes.Length - 7];
+                byte ReportType = FrameBytes[5];
+                for (int j = 0; j < ReportItem.Length; j++)
+                {
+                    ReportItem[i] = FrameBytes[i + 6];
+                }
+            }
+        }
+        #endregion 接收数据实际处理部分
+
+        #region 发送数据组织部分
+
+        #endregion 发送数据组织部分
+    }
+
+    public class FrameItem
+    {
+        public byte[] HeadByte = { 0x55, 0xAA };
+        //0x01	1	STM32向上位机发送请求报文
+        //0x10	1	上位机向STM32发送响应报文
+        //0x02	1	上位机向STM32发送请求报文
+        //0x20	1	STM32向上位机发送响应报文
+        /// <summary>
+        /// 帧类型（第三字节）
+        /// </summary>
+        public byte FrameType = 0x02;
+        /// <summary>
+        /// 报文类型（第四字节）
+        /// </summary>
+        /*
+         * §1.1.5.1	报警状态上报报文（01）
+         * §1.1.5.2	工序状态上报报文（02）
+         * §1.1.5.3	报警状态查询报文（03）
+         * §1.1.5.4	状态控制报文(04)
+         */
+        public byte ReportType = 0x01;//默认为报警状态上报报文
+
+        public byte[] FrameLenth = { 0x00, 0x00 };
+
+        public byte[] ReportItem = new byte[0];
+
+        public byte CRCResult = 0x00;
+    }
+}
